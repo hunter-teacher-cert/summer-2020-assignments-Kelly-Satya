757696433a128ab69a3f97002cfe717f32aa33db
Kelly-Satya <67009533+Kelly-Satya@users.noreply.github.com>
2020-06-19 10:07:45 -0400

    exercise 2 part 2

---
+++ pre02/Date.java
@@ -0,0 +1,19 @@
+import java.io.*;
+import java.util.*;
+
+public class Date{
+
+  public static void main(String[] args){
+    String day= "Friday,";
+    String month="June ";
+    int date= 19;
+    int year= 2020;
+
+    System.out.print("Happy Juneteenth! ");
+    System.out.print("Today is ");
+    System.out.print(month);
+    System.out.print(date);
+    System.out.print(", ");
+    System.out.println(year);
+  }
+}
