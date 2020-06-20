0c8a574db8b39c62676c5b91fd256c0d36fbe602
Kelly-Satya <67009533+Kelly-Satya@users.noreply.github.com>
2020-06-20 18:30:57 -0400

    Revised after reading Chap 3

--- pre02/Time.java
+++ pre02/Time.java
@@ -5,11 +5,17 @@ public class Time {
 
   public static void main(String[] args){
 
-    double hour= 8;
-    double minute=30;
-    double second=59;
-    double hourToMin= hour * 60;
-    double totalMin= hourToMin + minute;
+/*I tinkered with which variables can stay as int data types and which need to be double types; originally 
+perPassNum variable wasn't working because the data type was givng the compiler the impression it should carry 
+out integer division. I was trying to use the  printf with a format specification %.2f but couldn't figure out
+the syntax. I was hoping to use it to format the percentage printed for the perPass variable. */
+
+
+    int hour= 8;
+    int minute=30;
+    int second=59;
+    int hourToMin= hour * 60;
+    int totalMin= hourToMin + minute;
     double totalSec= totalMin * 60;
     double secRemainNum= 86400 - totalSec;
     double perPassNum= (totalSec / 86400) * 100;
