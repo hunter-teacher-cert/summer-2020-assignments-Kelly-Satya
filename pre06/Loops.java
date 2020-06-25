c99c59a4411bdc6969ad46915d8b1160ef43258a
Kelly-Satya <67009533+Kelly-Satya@users.noreply.github.com>
2020-06-25 15:49:33 -0400

    Excecise 5ish

--- pre06/Loops.java
+++ pre06/Loops.java
@@ -31,6 +31,20 @@ public static int factorial (int n) {
 
 }
 
+public static int myexp (int x, int n) {
+//My attempt at exercise 5
+      float ans=1;
+      int temp=x;
+      int stop=n;
+      int i;
+
+      for (i=1; i<=4; i++) {
+
+        temp=(temp*x)/i;
+        ans=ans+temp;
+      }
+        return myexp;
+}
 
   public static void main(String[] args) {
 
@@ -46,8 +60,17 @@ public static int factorial (int n) {
       System.out.println(factorial(3));
       System.out.println(factorial(12));
 
+//testing exercise 5
 
-
+      int stop;
+      int temp;
+      System.out.println(myexp(1,4));
+/*As much as I hate too, I'm giving up on this exercise. I don't understand the
+math yet. I looked at several videos about e^x infinite series expansion and I
+just feel completely overwhelmed by the combination of the math (I do NOT have a
+strong math background--I love math and I'm working on learning what I haven't
+learned yet in all my schooling) and the loads of additional coding points in this
+challenge. */
   }
 }
 //x^n=x*x^n-1
