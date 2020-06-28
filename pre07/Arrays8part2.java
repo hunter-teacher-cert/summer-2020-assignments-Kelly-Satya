b28c8ecaf0c1d4c87796be9d375208bf2b1492a6
Kelly-Satya <67009533+Kelly-Satya@users.noreply.github.com>
2020-06-28 16:57:39 -0400

    ex 1.2

--- pre07/Arrays8part2.java
+++ pre07/Arrays8part2.java
@@ -3,42 +3,53 @@ import java.util.*;
 
 
 class Arrays8part2 {
+//I've copied and pasted this randomArray method from Tsee
+  public static int[] randomArray(int size) {
+  		Random random = new Random();
+  		int[] a = new int[size];
+  		for (int i = 0; i < a.length; i++) {
+  			a[i] = random.nextInt(100);
+  		}
+  		return a;
+  	}
 
-public static int inRange(int[] a, int low, int high) {
-
-    int count=0;
-    for (int i=0; i <a.length; i++) {
-
-      if (a[i] >= low && a[i] <high) {
-        count++;
-      }
-    }
-      System.out.println(count);
-      return count;
-  }
-
-// public static int histogram(int[] a) {
-//     int[] counts= new int[100];
+// public static int inRange(int[] a, int low, int high) {
 //
-//     for (int score : a) {
+//     int count=0;
+//     for (int i=0; i <a.length; i++) {
 //
-//       counts[score]++;
+//       if (a[i] >= low && a[i] <high) {
+//         count++;
+//       }
 //     }
-//
-// return counts;
-// }
+//       System.out.println(count);
+//       return count;
+//   }
+
+public static int[] histogram(int[] a) {
+    int[] counts= new int[100];
+
+    for (int score : a) {
+
+      counts[score]++;
+    }
+System.out.println(counts);
+return counts;
+}
 
 
 public static void main(String[] arg){
 
-      int[] a= {97, 55, 72, 69, 86, 86, 84, 83, 88, 92, 93, 94, 86};
-      //histogram(a, 100);
+      int[] firstArray= randomArray(6);
+      int[] countsArray=histogram(firstArray);
+      System.out.println(Arrays.toString(firstArray));
+      System.out.println(Arrays.toString(countsArray));
 
-      int firstBar= inRange(a, 50, 59);
-      int secondBar=inRange(a, 60, 69);
-      int thirdBar= inRange(a, 70, 79);
-      int fourthBar=inRange(a, 80, 89);
-      int fifthBar=inRange(a, 90, 99);
+      // int firstBar= inRange(a, 50, 59);
+      // int secondBar=inRange(a, 60, 69);
+      // int thirdBar= inRange(a, 70, 79);
+      // int fourthBar=inRange(a, 80, 89);
+      // int fifthBar=inRange(a, 90, 99);
 
 }
 }
