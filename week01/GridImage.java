eeb90491dc943e31a51423b0e39b297622afa735
Kelly-Satya <67009533+Kelly-Satya@users.noreply.github.com>
2020-07-01 13:05:41 -0400

    clawing out of my lrng pit

--- week01/GridImage.java
+++ week01/GridImage.java
@@ -10,24 +10,25 @@ public class GridImage {
 
     int[][] array2d = new int[rows][cols];
 
-    rowPop(array2d,2,255);
-    colPop(array2d,3,255);
+    rowPop(array2d,2,14);
+    colPop(array2d,3,89);
 
+
+    invert(array2d);
     print2d(array2d);
-    System.out.println();
+    //System.out.println(invert);
 
 
-  }
+  } //end main
 
   public static void print2d(int[][] array2d) {
 
-    for (int row=0; row < array2d.length; row++) {
-      for (int col=0; col < array2d[row].length; col++) {
-        System.out.printf("%03d ", array2d[row][col]);
-      }//end c for
-
-      System.out.println();
-    }//end r for
+      for (int row=0; row < array2d.length; row++) {
+        for (int col=0; col < array2d[row].length; col++) {
+          System.out.printf("%03d ", array2d[row][col]);
+        }//end c for
+          System.out.println();
+      }//end r for
   }//end print2d
 
 
@@ -35,26 +36,37 @@ public class GridImage {
 
     //populates row in array2d with value
 
-    for (int col=0; col < array2d[row].length; col++) {
-      array2d[row][col]= value;
+        for (int col=0; col < array2d[row].length; col++) {
+            array2d[row][col]= value;
+        }//end row
 
-    }//end row
   }//end rowPop
 
   public static void colPop(int[][] array2d, int col, int value) {
 //populates column c in d2 with value
 
-      for (int ind=0; ind < array2d.length; ind++) {
-          array2d[ind][col]= value;
-        }
+        for (int ind = 0; ind < array2d.length; ind++) {
+            array2d[ind][col]= value;
+        } //end ind
 
   }//end colPop
 
   public static void invert(int[][] array2d) {
-
-    /*Goes through d2 and modifies each element so all 0s become 255 and
+    /*Goes through array2d and modifies each element so all 0s become 255 and
     any non-zero value becomes zero */
-  }//end invert
+    for (int row = 0; row < array2d.length ; row++ ) {
+        for (int col = 0; col < array2d[row].length; col++) {
+          if (array2d[row][col] > 0) {
+              array2d[row][col] = 0;
+          }// end if statement
+          else {
+              array2d[row][col] = 255;
+          }//end else
+          //System.out.print(array2d[row][col]);
+          }//end for second for loop
+      } //end for first loop
+
+  } //end invert
 
 
   public static void diagonal(int[][] array2d, int row, int col, int direction, int value) {
