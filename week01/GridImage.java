cb04bd096f3e3290600b404f50084b11a68cb95c
Kelly-Satya <67009533+Kelly-Satya@users.noreply.github.com>
2020-07-01 08:42:25 -0400

    Making Progres while weeping lolz

--- week01/GridImage.java
+++ week01/GridImage.java
@@ -8,19 +8,23 @@ public class GridImage {
     int rows = 20;
     int cols = 20;
 
-    int[][] grid = new int[rows][cols];
+    int[][] array2d = new int[rows][cols];
 
-    print2d(grid);
+    rowPop(array2d,2,255);
+    colPop(array2d,3,255);
+
+    print2d(array2d);
     System.out.println();
+
 
   }
 
-  public static void print2d(int[][] d2) {
+  public static void print2d(int[][] array2d) {
 
-    for (int r=0; r < d2.length; r++) {
-      for (int c=0; c < d2[r].length; c++) {
+    for (int row=0; row < array2d.length; row++) {
+      for (int col=0; col < array2d[row].length; col++) {
 
-        System.out.printf("%03d ", d2[r][c]);
+        System.out.printf("%03d ", array2d[row][col]);
       }//end c for
 
       System.out.println();
@@ -28,24 +32,33 @@ public class GridImage {
   }//end print2d
 
 
-  public static void rowPop(int[][] d2, int r, int value) {
+  public static void rowPop(int[][] array2d, int row, int value) {
 
-    //populates row r in d2 with value
+    //populates row in array2d with value
+
+    for (int col=0; col < array2d[row].length; col++) {
+      array2d[row][col]= value;
+
+    }//end row
   }//end rowPop
 
-  public static void colPop(int[][] d2, int c, int value) {
+  public static void colPop(int[][] array2d, int col, int value) {
 //populates column c in d2 with value
+
+      for (int ind=0; ind < array2d.length; ind++) {
+          array2d[ind][col]= value;
+        }
 
   }//end colPop
 
-  public static void invert(int[][] d2) {
+  public static void invert(int[][] array2d) {
 
     /*Goes through d2 and modifies each element so all 0s become 255 and
     any non-zero value becomes zero */
   }//end invert
 
 
-  public static void diagonal(int[][] d2, int r, int c, int direction, int value) {
+  public static void diagonal(int[][] array2d, int r, int c, int direction, int value) {
 
     /* Sets the diagonal starting at d2[r][c] going in direction to value
       0: up + left
