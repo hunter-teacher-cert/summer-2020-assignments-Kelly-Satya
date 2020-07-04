ed6c7e7e4792d4233a2ab649cf63603963897ee2
Kelly-Satya <67009533+Kelly-Satya@users.noreply.github.com>
2020-07-04 17:00:19 -0400

    added accessors and mutators

--- week01/week01_day03/Driver.java
+++ week01/week01_day03/Driver.java
@@ -4,6 +4,11 @@ import java.util.*;
 
 public class Driver{
   public static void main(String[] args) {
-Time t = new Time();
+
+/* The new object is created in main, as follows. When you invoke new, java
+creates the object and calls your constructor to initialize the instance
+variables */
+
+Time time = new Time(11,59,59.9);
   }
 }
--- week01/week01_day03/TimeClient.java
+++ week01/week01_day03/TimeClient.java
@@ -13,18 +13,37 @@ public int getHour() {
       return this.hour;
 } // end getHour accessor
 
+
 public int getMinute(){
 
       return this.minute;
 } // end getMinute accessor
+
 
 public int getSecond() {
 
       return this.second;
 }
 
+/* the following methods, formally known as mutators but often called setters
+allow TimeClient to modify the instance variables of time. The method that sets
+something, is usually called setSomething*/
+
+public void setHour(int hour) {
+
+      this.hour = hour;
+} //end setHour method
 
 
+public void setMinute(int minute){
+
+      this.minute = minute;
+} //end setMinute method
+
+public void setSecond(double second){
+      this.second = second;
+
+} //end setSecond method
 
 public static void main(String[] args) {
 
