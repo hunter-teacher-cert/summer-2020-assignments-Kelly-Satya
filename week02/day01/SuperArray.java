6a9d379791bd270903096e34c5df425b65d1ad77
Kelly-Satya <67009533+Kelly-Satya@users.noreply.github.com>
2020-07-06 15:23:24 -0400

    lastest version

--- week02/day01/SuperArray.java
+++ week02/day01/SuperArray.java
@@ -9,23 +9,30 @@ public class SuperArray {
 /* constructor that accepts no parameters. Sets the data array so it can hold
 ten elements and numberElement to zero*/
 
-public void superArray(){
+public SuperArray(){
 
   /* array is declared about and this.data points to this instance of the class*/
 
-  this.data = new int[10];
-  this.numberElements = 0;
+  data = new int[10];
+  numberElements = 0;
 
+
+  //System.out.println(Arrays.toString(data));
 } // end SuperArray constructor that declares and instantiates super boundary
 
-public int[] superArray(int[] size){
+
+
+public SuperArray(int[] size){
 
 /* Isn't this giong to create a new array? */
 
-    this.data = superArray(size);
-    return superArray(size);
+    data = new int[size];
+    numberElements = 0;
 
+    return data;
 } // end superArray constructor that creates dynamic internal array
+
+
 
 public void add(int value){
 
@@ -33,21 +40,69 @@ public void add(int value){
   this.numberElement= this.numberElements + value.
   IF there is no more room in the array, do nothing.
   */
-    this.numberElements = this.numberElements + value;
+    if (value + this.numberElements < this.data.length){
+        this.numberElements = this.numberElements + value;
 
-    if (this.numberElements == 10) {
+        //access first free space in the array, and insert value there
+    }
+    /* ideally, the value below should not be hard-coded because I want this to
+    be more dynamic*/
+    if (value + this.numberElements >= this.numberElements.length) {
       return;
     } //end if statement
 
-}
+} // end add method
+
+
+public int getValue(int index){
+
+  /* return value at location index from the array, IF index is past the last
+  element, return -1;*/
+    int value = data[index];
+
+        if (data[index] > data.length)
+        {
+          return -1;
+        }
+} // end getElement method
+
+
+public boolean isEmpty() {
+
+    if (SuperArray == this.data[0]) {
+      return true;
+    } // end if statement
+    else {
+      return false;
+    } // end else statment
+} // end isEmpty method
+
+
+public String toString(){
+
+  /*build an return a string with all the elements of the array in a printable
+  form*/
+  String arrayData = " ";
+  arrayData = arrayData + " " + data[i];
+
+  for (int i = 0; i < numberElements; i++) {
+
+    //arrayData.add[i];
+
+  } // end toString method
+  return arrayData;
+} // end toString method
+
 
 public static void main(String[] args) {
 
-
+    // how do I send a value to my constructor?
+    SuperArray sa= new SuperArray();
+    sa.add(2);
+    //System.out.println(Arrays.toString(sa));
+    //System.out.println(sa.length);
 
 } // end main
-
-
 
 
 } // end class
